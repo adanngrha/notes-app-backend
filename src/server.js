f92bafb3b68b37112483dbdaa2bdf0334a55e0ec
Adan Nugraha <contact.ardana@gmail.com>
2022-09-10 14:01:53 +0700

    restructuring project, routing, services

--- src/server.js
+++ src/server.js
@@ -1,21 +1,27 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
+// const notesPlugin = require('./notesPlugin');
 
 const init = async () => {
-const server = Hapi.server({
+  const server = Hapi.server({
     port: 5000,
-      host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-      routes: {
-        cors: {
-          origin: ['*'],
-        },
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
       },
-    });
+    },
+  });
 
-    server.route(routes);
+  // await server.register({
+  //   plugin: notesPlugin,
+  //   options: { notes: [] },
+  // });
 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
